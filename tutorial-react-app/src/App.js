3231e202c7a7827d8c104410d96c7caa075866c8
electrictangent <electrictangent@protonmail.com>
2023-02-12 16:27:04 +1300

    Added properties to component tablebody

--- tutorial-react-app/src/App.js
+++ tutorial-react-app/src/App.js
@@ -3,12 +3,32 @@ import Table from './Table';
 
 
 // Render class component Table
-class App extends React.Component {
+class App extends Component {
   render() {
+    // Pretend return array from a JSON based API
+    const characters = [
+        {
+        name: 'Charlie',
+        job: 'Janitor',
+        },
+        {
+        name: 'Mac',
+        job: 'Bouncer',
+        },
+        {
+        name: 'Dee',
+        job: 'Aspring actress',
+        },
+        {
+        name: 'Dennis',
+        job: 'Bartender',
+        },
+    ]
+
     return (
-      <div className="App">
+      <div className="container">
         <h1>Hello, React!</h1>
-        <Table /> 
+        <Table characterData={characters} /> 
       </div>
     )
   }
