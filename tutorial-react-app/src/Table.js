74376eb4b0c313d6f06e858f95cd85ed40e6cbba
electrictangent <electrictangent@protonmail.com>
2023-02-12 16:02:36 +1300

    Added simple components to Table class component

--- tutorial-react-app/src/Table.js
+++ tutorial-react-app/src/Table.js
@@ -1,34 +1,48 @@
 import React, { Component } from 'react';
 
+// Simple components
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    )
+}
+
+const TableBody = () => {
+    return (
+        <tbody>
+            <tr>
+                <td>Charlie</td>
+                <td>Janitor</td>
+            </tr>
+            <tr>
+                <td>Mac</td>
+                <td>Bouncer</td>
+            </tr>
+            <tr>
+                <td>Dee</td>
+                <td>Aspiring actress</td>
+            </tr>
+            <tr>
+                <td>Dennis</td>
+                <td>Bartender</td>
+            </tr>
+        </tbody>
+    )
+}
+
+// Custom class component
 class Table extends Component {
     render() {
         return (
             <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Job</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Charlie</td>
-                <td>Janitor</td>
-              </tr>
-              <tr>
-                <td>Mac</td>
-                <td>Bouncer</td>
-              </tr>
-              <tr>
-                <td>Dee</td>
-                <td>Aspiring actress</td>
-              </tr>
-              <tr>
-                <td>Dennis</td>
-                <td>Bartender</td>
-              </tr>
-            </tbody>
-          </table>
+                <TableHeader />
+                <TableBody />
+            </table>
         )
     }
 }
