1d77a5f718b37b894030451dd9faaaee5cd5db2e
electrictangent <electrictangent@protonmail.com>
2023-02-12 15:45:57 +1300

    Created App component

--- tutorial-react-app/src/index.js
+++ tutorial-react-app/src/index.js
@@ -1,6 +1,32 @@
-import React from 'react';
+// Following tutorial https://www.taniarascia.com/getting-started-with-react/
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'
 
-const myFirstElement = <h1>Hello React!</h1>
+/* const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const header1 = <h1>Hello React!</h1>
+const header2 = (
+  <ul className="list">
+    <li>List 1</li>
+    <li>List 2</li>
+    <li>List 3</li>
+  </ul>
+);
+
+root.render(header1);
+root.render(header2);
+ */
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+      </div>
+    )
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+root.render(<App/>);
